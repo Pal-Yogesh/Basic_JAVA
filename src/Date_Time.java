454dc8503293ce94b1c068fe548dcc24ea445504
Pal-Yogesh <yogeshpal5049@gmail.com>
2022-05-29 10:41:55 +0530

    String comparison lexicographically

--- src/Date_Time.java
+++ src/Date_Time.java
@@ -3,7 +3,7 @@ import java.time.LocalDate;
 class Result {
 
     public static String findDay(int month, int day, int year) {
-        
+
         int d = Integer.valueOf(day);
         int m = Integer.valueOf(month);
         int y = Integer.valueOf(year);
